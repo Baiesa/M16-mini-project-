d6ed136a557d8fd0ffb189522ab6f593e7226910
Your Namemain <baiesaziz@Baiess-MacBook-Air.local>
2024-08-02 09:14:54 -0400

    updated once again

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders E-commerce App title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/e-commerce app/i);
+  expect(titleElement).toBeInTheDocument();
 });

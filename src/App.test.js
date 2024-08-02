7f88022acf217430ede07a830847120d5e3eaf15
Your Namemain <baiesaziz@Baiess-MacBook-Air.local>
2024-08-02 10:38:31 -0400

    updated yml file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom'; 
 import App from './App';
 
 test('renders E-commerce App title', () => {
   render(<App />);
-  const titleElement = screen.getByText(/e-commerce app/i); 
+  const titleElement = screen.getByText(/e-commerce app/i);
   expect(titleElement).toBeInTheDocument();
 });
